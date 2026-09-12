60f69fce0f604996922ca8f0ce5af2fd201cc58d
JoniHaap <jonihaa@metropolia.fi>
2026-09-12 18:40:44 +0300

    päivitetty readme

--- Moduuli3/teht4.py
+++ Moduuli3/teht4.py
@@ -5,7 +5,7 @@ if vuosi % 4 == 0 and vuosi % 100 != 0 or vuosi % 400 == 0:
     print("vuosi", vuosi, "on karkausvuosi")
 # Osasin tässä kohtaa if vuosi % 4 == 0, mutta sitten en kyllä tajunnut miten tuo loppu kaava menee kirjotettuna. Paperilla kyllä osaisin.
 # googlailin ja kyselin gpt, mutta en jotenkin silti tajua tuota loppuosaa.
-# Pitää pytää selittämään rautalangasta
+# Pitää pytää selittämään rautalangasta, koodi oli visualstudion ite ehdottaman rivi kun kirjoitin tuon alun if vuosi % 4 == 0
 
 else:
     print("vuosi", vuosi, "ei ole karkausvuosi")
--- readme.md
+++ readme.md
@@ -14,7 +14,9 @@ En ollut ihan varma miten teht 5 tulostus tulee näkyä niin on tyylillä x,xx k
 
 ## Moduuli 3
 
-Tehty tehtävä 1-3
+Tehty tehtävä 1-4
+Nelos tehtävässä en kyllä vaan tajunnut miten tuo kaava kirjotetaan yhteen pötköön ja yritin kysellä googlelta ja gpt, mutta ei ne osannut kertoa
+tarpeeksi selkeästi aloitelijalle, tehtävän alun ja lopun ossasin, mutta jakaminen 400 ja 100 samaan en vaan tajunnut
 
 ## Moduuli 4
 
